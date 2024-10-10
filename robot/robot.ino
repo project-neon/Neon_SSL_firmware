613ec5ecdfb1b6ca155c815ba37ecbd90b91bb27
marcusholanda <41521915+marcusholanda@users.noreply.github.com>
2024-10-10 15:33:27 -0300

    updated code

--- robot/robot.ino
+++ robot/robot.ino
@@ -6,8 +6,10 @@
 int robot_id = 0;
 int id;
 int first_mark = 0, second_mark;
+int last = 0;
 
-bool stop;
+bool stop=0;
+volatile bool new_data=0;
 
 float v_l, v_a, th;
 float last_error = 0;
@@ -16,7 +18,7 @@ float error_sum = 0;
 const byte numChars = 64;
 char commands[numChars];
 char tempChars[numChars];
-
+char last_message[numChars];
 
 typedef struct struct_message{
   char message[64];
@@ -24,12 +26,11 @@ typedef struct struct_message{
 
 struct_message rcv_commands;
 
-
 void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
   memcpy(&rcv_commands, incomingData, sizeof(rcv_commands));
   // Update the structures with the new incoming data
-  
   strcpy(commands, rcv_commands.message);
+  new_data=1;
 }
 
 void send_power(float m1,float m2,float m3,float m4){
@@ -51,8 +52,8 @@ void motors_control(float x, float y, float angular) {
 }
 
 void setup() {
+  delay(100);
   Serial.begin(115200);
-
   // configurações comunicação
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -69,58 +70,64 @@ void setup() {
     return;
   }
 
-  esp_now_register_recv_cb(OnDataRecv());
-  
+  esp_now_register_recv_cb(OnDataRecv);
+
+  //Serial.print("ESP Board MAC Address:  ");
+  //Serial.println(WiFi.macAddress());
   // configuração mpu
-
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-
   //mpu_init();
+  delay(100);
 }
 
 void loop() {
   
   strcpy(tempChars, commands); // necessário para proteger a informação original
   //Serial.println(tempChars);
-  parseData();
+  if(new_data) parseData();
 
   second_mark = millis();
   //Serial.println(second_mark - first_mark);
-  if (second_mark - first_mark > 500) {
+  if (second_mark - first_mark > 300) {
     v_l = 0.00;
     v_a = 0.00;
     th = 0.00;
     last_error = 0;
     error_sum = 0;
     stop = true;
-
   }
-  
-  motors_control(v_l, v_a,th); //aplica os valores para os motores
+
+//  if(millis()-last > 1){
+//    last = millis();
+  if(!stop) motors_control(v_l, v_a,th); //aplica os valores para os motores
+//  } 
 }
 
 void parseData(){
     char * strtokIndx;
-  
-    strtokIndx = strtok(tempChars, ",");
+      strtokIndx = strtok(tempChars, ",");
     
-    while (strtokIndx != NULL){
+      while (strtokIndx != NULL){
         id = atoi(strtokIndx);
         
         if(id == robot_id){ 
+          new_data=0;
+          stop = 0;
           first_mark = millis();        
           strtokIndx = strtok(NULL, ",");  
           v_l = atof(strtokIndx);       
           strtokIndx = strtok(NULL, ",");         
           v_a = atof(strtokIndx);
           strtokIndx = strtok(NULL, ","); 
-       }
+        }
 
        else{
           strtokIndx = strtok(NULL, ",");     
           strtokIndx = strtok(NULL, ",");         
           strtokIndx = strtok(NULL, ","); 
-       }
-   } 
+        }
+    } 
+   
+
+  
+   
 }
---
+++ sender/sender.ino
@@ -0,0 +1,111 @@
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+
+
+// MAC Adress genérico para enviar os dados no canal selecionado
+uint8_t broadcast_adr[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+//==============
+
+const byte numChars = 64;
+char receivedChars[numChars];
+char tempChars[numChars];   
+boolean newData = false;     
+int id, count;
+
+typedef struct struct_message{
+  char message[numChars];
+  } struct_message;
+
+struct_message commands;
+
+//==============
+
+esp_now_peer_info_t peerInfo;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(2, OUTPUT);
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_start());
+  ESP_ERROR_CHECK(esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE));
+  esp_wifi_set_max_tx_power(84);
+
+
+  if (esp_now_init() != ESP_OK) 
+  {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  memcpy(peerInfo.peer_addr, broadcast_adr, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) 
+  {
+    Serial.println("Failed to add peer");
+    return;
+  }
+ 
+}
+
+//=============
+
+void loop() {
+  recvWithStartEndMarkers();
+  
+  if (newData == true){
+      strcpy(commands.message, receivedChars);
+      sendData();
+      newData = false;
+  }
+}
+
+//==============
+
+void recvWithStartEndMarkers(){
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char in;
+
+    while (Serial.available()){
+        //  Formato da mensagem::
+        //  <[id1],[v_l1],[v_a1],[id2],[v_l2],[v_a2],[id3],[v_l3],[v_a3]>
+        in = Serial.read();
+
+        if (recvInProgress == true){
+            if (in != endMarker){
+                receivedChars[ndx] = in;
+                ndx++;
+                if (ndx >= numChars){
+                    ndx = numChars - 1;
+                }
+            }
+            else{
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (in == startMarker){
+            recvInProgress = true;
+        }
+    }
+}
+
+
+void sendData(){   
+    // esse delay é necessário para que os dados sejam enviados corretamente
+    esp_err_t message = esp_now_send(broadcast_adr, (uint8_t *) &commands, sizeof(commands));
+    digitalWrite(2,HIGH);
+    delay(3);
+    digitalWrite(2,LOW);
+}
