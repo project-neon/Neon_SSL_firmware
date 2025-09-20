9e97bc9841a4b3b8de6ef6eaa07c1c35f7910319
Joao Pedro Leal <jpkleal@gmail.com>
2025-09-20 14:52:07 -0300

    wip: kicker block

---
+++ robot/kicker.h
@@ -0,0 +1,99 @@
+#define KICK_PIN    33
+#define CHARGE_PIN  18
+
+// regras de segurança
+const unsigned long KICK_COOLDOWN_MS   = 2000; // entre chutes
+const unsigned long TIME_AFTER_CHARGE  = 500;   // ms após desligar carga antes do chute
+const unsigned long TIME_BEFORE_CHARGE = 10; // ms após desligar carga antes do chute
+const unsigned long MIN_CHARGE_TIME_MS = 2000; // ms de carga antes de poder chutar
+
+
+static bool charge_enabled = false;
+static unsigned long charge_on_since_ms = 0;
+static unsigned long last_kick_ms = 0;
+static unsigned long can_kick_now_since = 0;
+
+
+void charge_on() {
+  if (!charge_enabled) {
+    digitalWrite(CHARGE_PIN, HIGH);
+    charge_enabled = true;
+    charge_on_since_ms = millis();
+//    Serial.println(F("CHARGE ON"));
+  }
+}
+
+void charge_off() {
+  if (charge_enabled) {
+    digitalWrite(CHARGE_PIN, LOW);
+    charge_enabled = false;
+//    Serial.println(F("CHARGE OFF"));
+  }
+}
+
+bool can_kick_now() {
+  unsigned long now = millis();
+
+  if (!charge_enabled) {
+//    Serial.println(F("[BLOCK] Carregamento OFF"));
+    return false;
+  }
+  if (now - charge_on_since_ms < MIN_CHARGE_TIME_MS) {
+//    Serial.print(F("[BLOCK] Carregando ha "));
+//    Serial.print(now - charge_on_since_ms);
+//    Serial.println(F(" ms (< 2000 ms)"));
+    return false;
+  }
+  if (now - last_kick_ms < KICK_COOLDOWN_MS) {
+//    Serial.print(F("[BLOCK] Cooldown restante: "));
+//    Serial.print(KICK_COOLDOWN_MS - (now - last_kick_ms));
+//    Serial.println(F(" ms"));
+    return false;
+  }
+  can_kick_now_since = millis();
+  return true;
+}
+
+uint32_t calc_power(int pot){
+  return (uint32_t) max(0, min(20000, (int) map(pot, 0, 9, 0, 20000)));
+}
+
+bool do_kick(uint32_t pulse_us) {
+  //if (!can_kick_now()) return false;
+
+  unsigned long now = millis()
+  // AGORA -  TEMPO DO CAN KICK > TIME_AFTER_CHARGE 
+  charge_off();
+
+  delay(TIME_AFTER_CHARGE);
+//  Serial.print(F("[KICK] Pulso de "));
+//  Serial.print(pulse_us);
+//  Serial.println(F(" us"));
+  digitalWrite(KICK_PIN, HIGH);
+  delayMicroseconds(pulse_us);
+  digitalWrite(KICK_PIN, LOW);
+
+  last_kick_ms = millis();
+
+  charge_on();
+
+  return true;
+}
+
+void setup_kicker() {
+//  Serial.begin(115200);
+  
+  pinMode(KICK_PIN, OUTPUT);
+  pinMode(CHARGE_PIN, OUTPUT);
+  digitalWrite(KICK_PIN, LOW);
+  digitalWrite(CHARGE_PIN, LOW);
+  charge_on();
+
+  last_kick_ms = millis() - KICK_COOLDOWN_MS;
+}
+
+void kicker_control(){
+  if((kick_time > 0) && can_kick_now()){
+    if (do_kick(calc_power(kick_time))) kick_time = 0;
+  }
+}
