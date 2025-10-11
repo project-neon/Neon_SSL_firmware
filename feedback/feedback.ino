85dc4ed2d57e550ca25d319bc449b619ed4982ed
Alexandre Fukamati <alekeiti@hotmail.com>
2025-10-11 11:27:07 -0300

    fix(feedback): timeout idle robots

--- feedback/feedback.ino
+++ feedback/feedback.ino
@@ -2,6 +2,7 @@
 
 #define REFRESH_RATE 500
 #define FB_PASSWORD 1500
+#define DISCONNECT_TIMEOUT_MS 8000
 
 #include <esp_now.h>
 #include <WiFi.h>
@@ -22,6 +23,8 @@ typedef struct struct_message {
 int n_robots = 0;
 
 int ids_connected[6] = {-1, -1 ,-1, -1, -1, -1};
+unsigned long last_seen_ms[6] = { 0, 0, 0, 0, 0, 0 };
+
 
 const byte num_chars = 64;
 
@@ -35,30 +38,40 @@ String last_id5_msg = "<-1,-1,-1.0>";
 fb_message FeedbackData;
 
 void updateIdsConnected(int id){
-  if (id == 0) ids_connected[0] = 0;
-  if (id == 1) ids_connected[1] = 1;
-  if (id == 2) ids_connected[2] = 2;
-  if (id == 3) ids_connected[3] = 3;
-  if (id == 4) ids_connected[4] = 4;
-  if (id == 5) ids_connected[5] = 5;
+  if (id >= 0 && id < 6) ids_connected[id] = id;
 }
 
-//verifica se o id esta conectado
-void updateNumberOfConnections(int id){
-  if (n_robots == 0){
-    updateIdsConnected(id);
-    n_robots = n_robots +1;
-    return;
+
+void updateNumberOfConnections() {
+  int count = 0;
+  for (int j = 0; j < 6; j++) {
+    if (ids_connected[j] != -1) count++;
   }
-  else{ 
-    for(int j = 0; j < 6; j++){
-      if (ids_connected[j] == id){
-        return;
+  n_robots = count;
+}
+
+
+void checkTimeouts() {
+  unsigned long now = millis();
+  for (int id = 0; id < 6; id++) {
+    if (ids_connected[id] != -1) {
+      if (now - last_seen_ms[id] > DISCONNECT_TIMEOUT_MS) {
+        ids_connected[id] = -1;
+        resetLastMsg(id);
       }
     }
-    n_robots = n_robots + 1;
-    updateIdsConnected(id);
   }
+  updateNumberOfConnections();
+}
+
+void resetLastMsg(int id){
+  String msg = "<-1,-1,-1.0>";
+  if      (id == 0) last_id0_msg = msg;
+  else if (id == 1) last_id1_msg = msg;
+  else if (id == 2) last_id2_msg = msg;
+  else if (id == 3) last_id3_msg = msg;
+  else if (id == 4) last_id4_msg = msg;
+  else if (id == 5) last_id5_msg = msg;
 }
 
 
@@ -75,10 +88,15 @@ void OnDataRecv(const esp_now_recv_info * mac, const uint8_t *incomingData, int 
   memcpy(&FeedbackData, incomingData, sizeof(FeedbackData));
   if(FeedbackData.password == FB_PASSWORD){
     int new_id = FeedbackData.id;
-    updateNumberOfConnections(new_id);
-    updateLastMsgReceived(new_id, FeedbackData.rssi, FeedbackData.battery);
+    if (new_id >= 0 && new_id < 6) {
+      last_seen_ms[new_id] = millis();
+      updateIdsConnected(new_id);
+      updateLastMsgReceived(new_id, FeedbackData.rssi, FeedbackData.battery);
+      updateNumberOfConnections();
+      }
   }
 }
+
 
 
 void setup() {
@@ -99,6 +117,8 @@ void loop() {
     delay(100);
     ESP.restart();
   }
+  checkTimeouts();
+
   if (n_robots != 0){
     String message = last_id0_msg + last_id1_msg + last_id2_msg + last_id3_msg + last_id4_msg + last_id5_msg;
     Serial.flush(); 
